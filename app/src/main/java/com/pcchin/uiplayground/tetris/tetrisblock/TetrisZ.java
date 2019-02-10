70def192a35d4e1cdc2d1612adfa286ea628a98f
PC Chin <chin.pengcheng@gmail.com>
2019-02-10 16:34:18 +0800

    Finish all Tetris blocks

--- app/src/main/java/com/pcchin/uiplayground/tetris/tetrisblock/TetrisZ.java
+++ app/src/main/java/com/pcchin/uiplayground/tetris/tetrisblock/TetrisZ.java
@@ -14,18 +14,50 @@ public class TetrisZ extends TetrisBlock {
 
     @Override
     void setStartingCoords() {
-
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                ArrayList<Integer> tempList = new ArrayList<>();
+                tempList.add((TetrisSurfaceView.GRID_TOTAL_X/2) + i + j - 1);
+                tempList.add(i);
+                this.currentBlockCoords.add(tempList);
+            }
+        }
     }
 
     @Override
     ArrayList<Integer> getCtrGrid() {
-        return null;
+        return this.currentBlockCoords.get(1);
     }
 
     @Override
     public void rotate() {
         ArrayList<Integer> ctrGrid = this.getCtrGrid();
         ArrayList<ArrayList<Integer>> returnList = new ArrayList<>();
-        this.swapDir(returnList);
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                // S and Z Block structured differently because S has different ctr blocks
+                ArrayList<Integer> tempList = new ArrayList<>();
+                if (this.block_dir == TetrisBlock.DIR_UP) {
+                    // Change to horizontal
+                    tempList.add(ctrGrid.get(0) - 1 + i + j);
+                    tempList.add(ctrGrid.get(1) + i);
+                } else {
+                    // Change to vertical
+                    tempList.add(ctrGrid.get(0) - i);
+                    tempList.add(ctrGrid.get(1) - 1 + i + j);
+                }
+                returnList.add(tempList);
+            }
+        }
+
+        if (! this.checkCollision(returnList)) {
+            if (this.block_dir == TetrisBlock.DIR_UP) {
+                this.block_dir = TetrisBlock.DIR_LEFT;
+            } else {
+                this.block_dir = TetrisBlock.DIR_UP;
+            }
+            this.currentBlockCoords = returnList;
+        }
     }
 }
